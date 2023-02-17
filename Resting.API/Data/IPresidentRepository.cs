74a50bee68a146f8d754ce3a28dcfd4fe6546fee
Björn Noctiluca <bjorn.noctiluca@outlook.com>
2023-02-17 15:20:49 +0100

    flattened history

---
+++ Resting.API/Data/IPresidentRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Resting.API.Controllers;
+
+namespace Resting.API.Data
+{
+  public interface IPresidentRepository
+  {
+    President GetOne(string id);
+    IEnumerable<President> GetAll();
+    President Create(string name, string from, string to);
+    void Delete(string id);
+    President Update(string id, string name, string from, string to);
+  }
+}
